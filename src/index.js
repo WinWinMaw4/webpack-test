fa5d1848ea1baddfc105a0aa879e6a22cd4dfb6c
unknown <mawinwinmaw4@gmail.com>
2022-05-13 07:16:24 +0630

    basic

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import _ from 'lodash';
+// import join from "lodash/join";
+// import Modal from "bootstrap/js/src/modal"
+// import './index.scss'
+
+
+// let name = "hein htet zan";
+// console.log(name);
+//
+// let text = join(["apple","orange"],"_");
+// console.log(text);
+//
+// let modal = new Modal(document.getElementById("aa"));
+
+import 'bootstrap'
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+
+    entry: [
+        './src/index.js',
+        './src/index.scss'
+    ],
+    mode: "development",
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            // `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+};
+
+
+// MiniCssExtractPlugin,minicss,Optimizing
